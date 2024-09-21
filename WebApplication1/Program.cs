35f1e9ebed8b29f2c58bab7c2d4db15a8f1a2f3f
salmafareed998 <salmafareed998@gmail.com>
2024-09-22 00:30:22 +0300

    Part01 - Repository Pattern

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,6 @@
 using Company.Data.Contexts;
+using Company.Repository.Interfaces;
+using Company.Repository.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1
@@ -16,7 +18,7 @@ namespace WebApplication1
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
-
+            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
